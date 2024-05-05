4d3893a4aed403f9db4e80b04dad23eb64cef37b
ashiq012 <ashiqali9136@gmail.com>
2024-05-05 15:47:22 +0530

    Redux todo-app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>React Redux ToolKit</h1>
+     <Todos/>
+     <AddTodo/>
+    </>
+  )
+}
+
+export default App
